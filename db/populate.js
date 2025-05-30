0ca734b6c2ea8882c0c92ef0bc262edee9a2a9cf
sharpslice <davidlein1440p@gmail.com>
2025-05-29 22:21:05 -0700

    fix: schema and seeding fixed

--- db/populate.js
+++ db/populate.js
@@ -1,9 +1,20 @@
 const {Client} = require("pg");
-
+require('dotenv').config();
 const SQL = `
+
+    CREATE TABLE IF NOT EXISTS region(
+        id SERIAL PRIMARY KEY,
+        region varchar(100) NOT NULL
+    );
+
     CREATE TABLE IF NOT EXISTS trainer (
         id SERIAL PRIMARY KEY,
+        name varchar(100) NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS pokemon(
+        id SERIAL PRIMARY KEY,
         name varchar(100) NOT NULL,
+        region_id INTEGER REFERENCES region(id)
     );
 
     CREATE TABLE IF NOT EXISTS trainer_pokemon(
@@ -11,37 +22,26 @@ const SQL = `
         trainer_id INTEGER REFERENCES trainer(id),
         pokemon_id INTEGER REFERENCES pokemon(id),
         nickname varchar(100) NOT NULL,
-        level INTEGER NOT NULL,
+        level INTEGER NOT NULL
     
     );
-    
-    CREATE TABLE IF NOT EXISTS pokemon(
+    CREATE TABLE IF NOT EXISTS moves(
         id SERIAL PRIMARY KEY,
-        name varchar(100) NOT NULL,
-        region_id INTEGER REFERENCES region(id),
+        name varchar(100)
     );
 
-    CREATE TABLE IF NOT EXISTS region(
-        id SERIAL PRIMARY KEY,
-        region varchar(100) NOT NULL,
-    )
 
     CREATE TABLE IF NOT EXISTS pokemon_moveset(
         pokemon_id INTEGER REFERENCES pokemon(id),
         moves_id INTEGER REFERENCES moves(id),
         PRIMARY KEY (pokemon_id,moves_id)
-    )
-
-    CREATE TABLE IF NOT EXISTS moves(
-        id SERIAL PRIMARY KEY,
-        name varchar(100),
-    )
+    );
 
     CREATE TABLE IF NOT EXISTS learned_moves(
         trainer_pokemon_id INTEGER REFERENCES trainer_pokemon(id),
         moves_id INTEGER REFERENCES moves(id),
-        PRIMARY KEY (trainer_pokemon_id, moves_id),
-    )
+        PRIMARY KEY (trainer_pokemon_id, moves_id)
+    );
 
 `;
 
@@ -50,11 +50,11 @@ const SQL = `
 async function main(){
     console.log("seeding...");
     const client = new Client({
-        connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:
-        ${process.env.DB_PORT}/${DB_DATABASE}`,
+        connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
     });
     await client.connect();
     await client.query(SQL);
     await client.end()
     console.log("done")
 }
+main();
