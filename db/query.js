65a5b3994d103e6595ac89838167cf2a17f856d1
sharpslice <davidlein1440p@gmail.com>
2025-06-04 17:46:37 -0700

    feat: query add trainer

--- db/query.js
+++ db/query.js
@@ -1,1 +1,20 @@
 const pool = require('./pool');
+
+require('dotenv').config();
+
+async function addTrainer(name){
+    await pool.query(`
+            INSERT INTO trainer (name)
+            VALUES ($1)
+        
+        `,[name])
+}
+
+
+
+
+async function main(){
+    await addTrainer("david")
+    await pool.end()
+}
+main();
