a00fba64ffb86b21620d09b031854b8d434b7f3a
sharpslice <davidlein1440p@gmail.com>
2025-07-11 18:32:53 -0700

    style: added background color and icon to moveset

--- frontend/src/PokemonInfo/moves/Tile.jsx
+++ frontend/src/PokemonInfo/moves/Tile.jsx
@@ -1,13 +1,21 @@
+
+import { getTypeIcon } from "../Types/typesToIcon"
+
+
 function Tile({id,move,setSelectedTile}){
+
+    
     const onSelectClick=()=>{
         setSelectedTile(id)
-        console.log("tile: "+id)
+        
     }
 
     return(
         
-        <div className='moveset-panel__tile' onClick={onSelectClick}>
-            {move? move.name:'empty'}
+        <div className={'moveset-panel__tile'} style={{backgroundColor: move?getTypeIcon(move.type).color:'white' }} onClick={onSelectClick}>
+            <span className="moveset-panel__tile-text" style={{color: move?'white':'black'}}>
+                {move? move.name:'empty'}
+            </span>
 
         </div>
        
